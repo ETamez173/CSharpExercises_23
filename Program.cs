28068daf065876463e11dfec6d5df21737ad16e2
ETamez173 <56414835+ETamez173@users.noreply.github.com>
2021-03-25 19:43:55 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CSharpExercises_23
+{
+    class Program
+    {
+
+        // stuff here
+
+        /* stuff here 
+           stuff here
+           stuff here
+        */
+
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
